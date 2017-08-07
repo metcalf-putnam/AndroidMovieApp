d1d0e13026a4f399af0a873be2c0c8676a386817
Patrice Metcalf-Putnam <patrice.metcalf.putnam@gmail.com>
2017-08-07 08:11:55 -0400

    added reviews and beginning of trailers, layout changes

--- app/src/main/java/com/example/android/movieapp/MainActivity.java
+++ app/src/main/java/com/example/android/movieapp/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.android.movieapp;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
+
 import com.example.android.movieapp.data.MoviePreferences;
 import com.example.android.movieapp.utilities.NetworkUtils;
 import com.example.android.movieapp.utilities.ParseMovieJsonUtils;
@@ -19,82 +25,133 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-        implements RecyclerAdapter.MoviePosterClickListener{
-    private int mMovieListSize;
+        implements RecyclerAdapter.MoviePosterClickListener, LoaderManager.LoaderCallbacks<ArrayList<Movie>>{
+    private int mMovieListSize = 0;
     private RecyclerAdapter mAdapter;
     private String mPreferredSearch;
-    private int mPageResults;
+    private int mPageResults = 1;
+    private static final int MOVIE_DB_SEARCH_LOADER = 15;
+    private static final String SEARCH_EXTRA = "searchExtra";
+    private static final String PAGE_RESULTS_EXTRA = "pages";
+    private static final String MOVIE_LIST_SIZE_EXTRA = "listSize";
+    private static final String MOVIE_RESULTS_EXTRA = "movieList";
+    private static final String PREFERRED_SEARCH_EXTRA = "preferred";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdapter = new RecyclerAdapter(this);
         RecyclerView moviePosterGrid;
-        mMovieListSize = 0;
-        mPageResults = 1;
         moviePosterGrid = (RecyclerView) findViewById(R.id.rv_movies);
-        GridLayoutManager layoutManager = new GridLayoutManager(this, 4);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 4); //hardcoded number;;need change
         moviePosterGrid.setLayoutManager(layoutManager);
         moviePosterGrid.setHasFixedSize(true);
+        moviePosterGrid.setAdapter(mAdapter);
+        mPreferredSearch = MoviePreferences.getPreferred();
+
+        if(savedInstanceState != null){
+            mPageResults = savedInstanceState.getInt(PAGE_RESULTS_EXTRA);
+            mMovieListSize = savedInstanceState.getInt(MOVIE_LIST_SIZE_EXTRA);
+            ArrayList<Movie> movies = savedInstanceState.getParcelableArrayList(MOVIE_RESULTS_EXTRA);
+            mAdapter.setMovieData(movies);
+            mPreferredSearch = savedInstanceState.getString(PREFERRED_SEARCH_EXTRA);
+        }
+        if(!checkConnection()) {
+            return; //not connected
+        }
+
         moviePosterGrid.setOnScrollListener(new RecyclerView.OnScrollListener(){
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                GridLayoutManager gridLayout = (GridLayoutManager) recyclerView.getLayoutManager();
-                int lastPosition = gridLayout.findLastVisibleItemPosition() + 5;
-                if(mMovieListSize != 0){
-
-                    if(lastPosition >= mMovieListSize){
-                        new fetchMovieData().execute(mPreferredSearch);
+                if(checkConnection()) {
+                    GridLayoutManager gridLayout = (GridLayoutManager) recyclerView.getLayoutManager();
+                    int lastPosition = gridLayout.findLastVisibleItemPosition() + 5;
+                    if (mMovieListSize != 0) {
+                        if (lastPosition >= mMovieListSize) {
+                            callLoader();
+                        }
                     }
                 }
             }
         });
-        mAdapter = new RecyclerAdapter(this);
-        moviePosterGrid.setAdapter(mAdapter);
 
-        mPreferredSearch = MoviePreferences.getPreferred();
 
-        new fetchMovieData().execute(mPreferredSearch);
+        callLoader();
     }
 
-    private class fetchMovieData extends AsyncTask<String, Void, ArrayList<Movie>>{
+    private void callLoader(){
+        Bundle queryBundle = new Bundle();
+        queryBundle.putString(SEARCH_EXTRA, mPreferredSearch);
+        LoaderManager loaderManager = getSupportLoaderManager();
 
-        @Override
-        protected ArrayList<Movie> doInBackground(String... params) {
-            ArrayList<Movie> movieArray;
-            movieArray = new ArrayList<>();
+        Loader<ArrayList<Movie>> movieLoader = loaderManager.getLoader(MOVIE_DB_SEARCH_LOADER);
 
-            if (params.length == 0) {
+        if(movieLoader == null){
+            loaderManager.initLoader(MOVIE_DB_SEARCH_LOADER, queryBundle, this);
+        }
+        else{
+            loaderManager.restartLoader(MOVIE_DB_SEARCH_LOADER, queryBundle, this);
+        }
+    }
+    @Override
+    public Loader<ArrayList<Movie>> onCreateLoader(int id, final Bundle args) {
+        if(!checkConnection()){
+            return null;
+        }
+        return new AsyncTaskLoader<ArrayList<Movie>>(this) {
+
+            @Override
+            protected void onStartLoading() {
+                super.onStartLoading();
+                if(args == null){
+                    return;
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public ArrayList<Movie> loadInBackground() {
+                String searchInfo = args.getString(SEARCH_EXTRA);
+                if(searchInfo == null || TextUtils.isEmpty(searchInfo)){
+                    return null;
+                }
+
+                ArrayList<Movie> movieArray;
+                movieArray = new ArrayList<>();
+                URL url = NetworkUtils.buildBaseSortURL(searchInfo, mPageResults);
+                String jsonData;
+                if(NetworkUtils.isNetworkAvailable(MainActivity.this)) {
+                    try {
+                        jsonData = NetworkUtils.getResponseFromHttpUrl(url);
+
+                        movieArray = ParseMovieJsonUtils.getSimpleMovieStringsFromJson(jsonData);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    mPageResults++;
+                    return movieArray;
+                }
                 return null;
             }
-            String preferredSearch = params[0];
-            URL url = NetworkUtils.buildBaseSortURL(preferredSearch, mPageResults);
-            String jsonData;
+        };
+    }
 
-            try {
-                jsonData = NetworkUtils.getResponseFromHttpUrl(url);
-                movieArray = ParseMovieJsonUtils.getSimpleMovieStringsFromJson(jsonData);
+    @Override
+    public void onLoadFinished(Loader<ArrayList<Movie>> loader, ArrayList<Movie> movieData) {
 
-            } catch (IOException e){
-                e.printStackTrace();
-            } catch (JSONException e){
-                e.printStackTrace();
-            }
-            mPageResults++;
-            return movieArray;
+        if(movieData != null){
+            mMovieListSize = mAdapter.setMovieData(movieData);
         }
+    }
 
-        @Override
-        protected void onPostExecute(ArrayList<Movie> movieData) {
-
-            if(movieData != null){
-                String output = "";
-                for(Movie m: movieData) {
-                    output += m.toString();
-                }
-                mMovieListSize = mAdapter.setMovieData(movieData);
-            }
-        }
+    @Override
+    public void onLoaderReset(Loader<ArrayList<Movie>> loader) {
 
     }
 
@@ -120,28 +177,48 @@ public class MainActivity extends AppCompatActivity
         int clickedItemId = item.getItemId();
         String mostPopular = NetworkUtils.getMostPopularMoviesSearch();
         String topRated = NetworkUtils.getTopRatedMoviesSearch();
-
-        if(clickedItemId == R.id.menu_popular && !mPreferredSearch.equals(mostPopular)){
-            mPreferredSearch = mostPopular;
+        switch(clickedItemId){
+            case R.id.menu_popular:
+                mPreferredSearch = mostPopular;
+                break;
+            case R.id.menu_top_rated:
+                mPreferredSearch = topRated;
+                break;
+            case R.id.menu_favorites: //need to update this
+                break;
+        }
+        if(checkConnection()){
             resetSearch();
-            new fetchMovieData().execute(mPreferredSearch);
-            return true;
+            callLoader();
         }
-
-        else if(clickedItemId == R.id.menu_top_rated && !mPreferredSearch.equals(topRated)){
-            mPreferredSearch = topRated;
-            resetSearch();
-            new fetchMovieData().execute(mPreferredSearch);
-            return true;
-        }
-        else {
-            Toast.makeText(this, R.string.already_selected, Toast.LENGTH_SHORT).show();
-            return super.onOptionsItemSelected(item);
-        }
+        return super.onOptionsItemSelected(item);
     }
     private void resetSearch(){
         mMovieListSize = 0;
-        mAdapter.clearMovieData();
+        if(mAdapter != null) {
+            mAdapter.clearMovieData();
+        }
         mPageResults = 1;
+    }
+    private boolean checkConnection(){
+        TextView error = (TextView) findViewById(R.id.tv_error);
+        if(NetworkUtils.isNetworkAvailable(this)){
+            error.setVisibility(View.INVISIBLE);
+            return true;
+        }else{
+            error.setText(R.string.error_no_internet);
+            error.setVisibility(View.VISIBLE);
+            return false;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PAGE_RESULTS_EXTRA, mPageResults);
+        outState.putInt(MOVIE_LIST_SIZE_EXTRA, mMovieListSize);
+        outState.putString(PREFERRED_SEARCH_EXTRA, mPreferredSearch);
+        outState.putParcelableArrayList(MOVIE_RESULTS_EXTRA, mAdapter.getMoviesArray());
     }
 }
--- app/src/main/java/com/example/android/movieapp/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/movieapp/utilities/NetworkUtils.java
@@ -1,7 +1,12 @@
 package com.example.android.movieapp.utilities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.widget.TextView;
 
+import com.example.android.movieapp.MainActivity;
 import com.example.android.movieapp.R;
 
 import java.io.IOException;
@@ -16,7 +21,7 @@ import java.util.Scanner;
  */
 
 public class NetworkUtils {
-    private static final String MY_API_KEY = "YOUR_API_KEY_HERE";
+    private static final String MY_API_KEY = "boogiepuppeh";
 
     private static final String BASE_MOVIE_SEARCH_URL = "api.themoviedb.org";
     private static final String AUTH_TYPE = "3";
@@ -28,6 +33,8 @@ public class NetworkUtils {
 
     private static final String MOST_POPULAR_MOVIES_SEARCH = "popular";
     private static final String TOP_RATED_MOVIES_SEARCH = "top_rated";
+    private static final String REVIEWS_SEARCH = "reviews";
+    private static final String TRAILERS_SEARCH = "videos";
 
     private static final String BASE_MOVIE_POSTER_URL = "http://image.tmdb.org/t/p/";
     private static final String POSTER_SIZE = "w185/";
@@ -36,6 +43,45 @@ public class NetworkUtils {
         return MOST_POPULAR_MOVIES_SEARCH;
     }
     public static String getTopRatedMoviesSearch() { return TOP_RATED_MOVIES_SEARCH; }
+
+    public static String getReviews(String movieId){
+        URL reviewUrl = buildDetailedMovieSearchUrl(movieId, REVIEWS_SEARCH);
+        String reviews = null;
+        try {
+            reviews = getResponseFromHttpUrl(reviewUrl);
+        }catch (IOException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return reviews;
+    }
+
+    public static String getTrailers(String movieId){
+        URL trailerUrl = buildDetailedMovieSearchUrl(movieId, TRAILERS_SEARCH);
+        String trailers = null;
+        try{
+            trailers = getResponseFromHttpUrl(trailerUrl);
+        }catch(IOException e){
+            e.printStackTrace();
+            return e.getMessage();
+        }
+        return trailers;
+    }
+    private static URL buildDetailedMovieSearchUrl(String movieId, String query){
+        Uri.Builder builder = new Uri.Builder();
+
+        builder.scheme("https")
+                .authority(BASE_MOVIE_SEARCH_URL)
+                .appendPath(AUTH_TYPE)
+                .appendPath(MEDIA_TYPE_MOVIE)
+                .appendPath(movieId)
+                .appendPath(query)
+                .appendQueryParameter(API_BASE, MY_API_KEY)
+                .appendQueryParameter(LANGUAGE, ENGLISH_DEFAULT)
+                .appendQueryParameter(PAGE, "1")
+                .build();
+        return makeUrlFromString(builder.toString());
+    }
 
     public static URL buildBaseSortURL(String query, int page){
         Uri.Builder builder = new Uri.Builder();
@@ -50,6 +96,10 @@ public class NetworkUtils {
                 .appendQueryParameter(LANGUAGE, ENGLISH_DEFAULT)
                 .appendQueryParameter(PAGE, pageString)
                 .build();
+        return makeUrlFromString(builder.toString());
+    }
+
+    private static URL makeUrlFromString(String builder){
         URL url = null;
         try{
             url = new URL(builder.toString());
@@ -59,24 +109,43 @@ public class NetworkUtils {
         }
         return url;
     }
-    public static String getResponseFromHttpUrl(URL url) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
 
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+    private static String getMessageFromStream(InputStream stream) {
+        Scanner scanner = new Scanner(stream);
+        scanner.useDelimiter("\\A");
 
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) {
-                return scanner.next();
-            } else {
-                return null;
-            }
-        } finally {
-            urlConnection.disconnect();
+        boolean hasInput = scanner.hasNext();
+        if (hasInput) {
+            return scanner.next();
+        } else {
+            return null;
         }
     }
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setConnectTimeout(5 * 1000);
+        urlConnection.setReadTimeout(7 * 1000);
+            try {
+                InputStream in = urlConnection.getInputStream();
+                return getMessageFromStream(in);
+            } catch (IOException e) {
+                InputStream err = urlConnection.getErrorStream();
+                String errResponse = getMessageFromStream(err);
+                throw new IOException(errResponse);
+            } finally {
+                urlConnection.disconnect();
+            }
+    }
+
+    public static boolean isNetworkAvailable(Context context) {
+        // Using ConnectivityManager to check for Network Connection
+        ConnectivityManager connectivityManager = (ConnectivityManager) context
+                .getSystemService(context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager
+                .getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    } //This function modified from http://www.androidbegin.com/tutorial/android-check-internet-connection/
 
     public static String getFullPosterPath(String posterPath){
         return BASE_MOVIE_POSTER_URL + POSTER_SIZE + posterPath;
